11524ebbb099e11878e6983aa5c6f993fa7b3e8b
Shreyansh <shreyansh7a@gmail.com>
2025-05-08 22:34:44 -0400

    both backend and frontend working

--- fastapi/main.py
+++ fastapi/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # CORS (optional, add origins if needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Update if needed
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],  # Update if needed
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
